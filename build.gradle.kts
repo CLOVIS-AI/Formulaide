e287b8ed18cd2cacea72174eefa8007068b958b5
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-02 14:40:18 +0200

    build(gradle): Configured project group & version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
 	kotlin("jvm") version Version.kotlin apply false
 	kotlin("plugin.serialization") version Version.kotlin apply false
 }
+
+group = "fr.ville-arcachon"
+version = "1.0-SNAPSHOT"
+
+subprojects {
+	group = rootProject.group
+	version = rootProject.version
+}
