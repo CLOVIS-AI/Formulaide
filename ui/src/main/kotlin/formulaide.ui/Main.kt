70e1f7d7f722edd9f32a7b46ceaf55cd23962a28
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-03 21:05:59 +0200

    feat(ui): React boilerplate page

--- ui/src/main/kotlin/formulaide.ui/Main.kt
+++ ui/src/main/kotlin/formulaide.ui/Main.kt
@@ -1,7 +1,15 @@
 package formulaide.ui
 
+import kotlinx.browser.document
+import react.dom.h1
+import react.dom.render
+
 val helloWorld get() = "Hello World!"
 
 fun main() {
-	println(helloWorld)
+	render(document.getElementById("root")) {
+		h1 {
+			+helloWorld
+		}
+	}
 }
