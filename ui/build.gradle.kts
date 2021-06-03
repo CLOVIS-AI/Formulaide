1ae1dfb9d40547e83b607a7f48349effd0800891
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-03 20:46:50 +0200

    upgrade(ui): Added React and React DOM 17.0.2

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -19,4 +19,9 @@ dependencies {
 	testImplementation(kotlin("test-js"))
 
 	implementation(project(":client"))
+
+	implementation(kotlinWrapper("react", Version.kotlinReact))
+	implementation(kotlinWrapper("react-dom", Version.kotlinReact))
+	implementation(npm("react", Version.react))
+	implementation(npm("react-dom", Version.react))
 }
