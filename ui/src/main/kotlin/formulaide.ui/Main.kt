e177225cb36f54bc643560957bcdd5d4ce309044
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-04 11:36:20 +0200

    test(ui): Do not display the UI in tests

--- ui/src/main/kotlin/formulaide.ui/Main.kt
+++ ui/src/main/kotlin/formulaide.ui/Main.kt
@@ -1,5 +1,6 @@
 package formulaide.ui
 
+import formulaide.ui.utils.detectTests
 import kotlinx.browser.document
 import react.dom.h1
 import react.dom.render
@@ -7,6 +8,9 @@ import react.dom.render
 val helloWorld get() = "Hello World!"
 
 fun main() {
+	if (detectTests())
+		return
+
 	render(document.getElementById("root")) {
 		h1 {
 			+helloWorld
