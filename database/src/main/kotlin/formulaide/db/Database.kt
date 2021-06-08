eccce27f72d26998781d8c8fa87efe740433f6de
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-08 10:33:36 +0200

    feat(database): Connect to MongoDB

---
+++ database/src/main/kotlin/formulaide/db/Database.kt
@@ -0,0 +1,24 @@
+package formulaide.db
+
+import formulaide.db.document.DbUser
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+/**
+ * Abstraction over the database connection.
+ *
+ */
+class Database(
+	host: String,
+	port: Int,
+	databaseName: String,
+	username: String,
+	password: String,
+) {
+
+	private val client = KMongo.createClient("mongodb://$username:$password@$host:$port").coroutine
+	private val database = client.getDatabase(databaseName)
+
+	internal val users = database.getCollection<DbUser>("users")
+
+}
