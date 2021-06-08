a892b3adf89a34d86c14a6430075f16ff154eca6
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-08 10:35:37 +0200

    feat(database): Connect to the database when the server starts

--- server/src/main/kotlin/formulaide/server/Main.kt
+++ server/src/main/kotlin/formulaide/server/Main.kt
@@ -1,12 +1,22 @@
 package formulaide.server
 
+import formulaide.db.Database
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 val message get() = "Hello World!"
+lateinit var database: Database
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+	println("The server is starting…")
+
+	println("Connecting to the database…")
+	database = Database("localhost", 27017, "formulaide", "root", "development-password") //TODO: inject params
+
+	println("Starting Ktor…")
+	io.ktor.server.netty.EngineMain.main(args)
+}
 
 fun Application.formulaide(testing: Boolean = false) {
 	routing {
