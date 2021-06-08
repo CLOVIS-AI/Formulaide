a3b73816cbe81e44f8ebcb1ec44c06257bd6e0af
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-08 12:20:07 +0200

    tests(database): Test that the DB refuses duplicate inserts for users

--- database/src/test/kotlin/formulaide/db/UsersTest.kt
+++ database/src/test/kotlin/formulaide/db/UsersTest.kt
@@ -5,9 +5,7 @@ import formulaide.db.document.createUser
 import formulaide.db.document.findUser
 import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import kotlin.test.*
 
 class UsersTest {
 
@@ -15,10 +13,37 @@ class UsersTest {
 	fun createUser() = runBlocking {
 		val db = testDatabase()
 
-		val expected = DbUser("123456", "random@gmail.fr", "123456789", "My Other Name", db.testService().id, false)
+		val expected = DbUser(
+			"123456",
+			"random${Random.nextInt()}@gmail.fr",
+			"123456789",
+			"My Other Name",
+			db.testService().id,
+			false
+		)
 
 		val actual = db.createUser(expected)
 		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun createDuplicateUser() = runBlocking {
+		val db = testDatabase()
+
+		val user = DbUser(
+			"123456",
+			"r${Random.nextInt()}@gmail.fr",
+			"123456789",
+			"Name",
+			db.testService().id,
+			false
+		)
+		db.createUser(user)
+
+		val failure = assertFails {
+			db.createUser(user)
+		}
+		assertTrue(failure is IllegalStateException)
 	}
 
 	@Test
