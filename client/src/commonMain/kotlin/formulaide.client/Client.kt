0bde2aba33189ceea5ffd05d1e0e7852b9137319
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-09 10:22:03 +0200

    feat(client): Difference between anonymous and connected clients

--- client/src/commonMain/kotlin/formulaide.client/Client.kt
+++ client/src/commonMain/kotlin/formulaide.client/Client.kt
@@ -3,16 +3,17 @@ package formulaide.client
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 
-class Client(private val hostUrl: String) {
+/**
+ * Common behavior between [AnonymousClient] and [AnonymousClient].
+ */
+sealed class Client(internal val hostUrl: String) {
 
-	internal val client = HttpClient {
-		install(JsonFeature) {
-			serializer = KotlinxSerializer()
-		}
-	}
+	internal abstract val client: HttpClient
+	internal open val config: HttpRequestBuilder.() -> Unit = {}
 
 	/**
 	 * Makes an HTTP request to the server.
@@ -25,6 +26,7 @@ class Client(private val hostUrl: String) {
 	): Out {
 		return client.request(hostUrl + url) {
 			this.method = method
+			config()
 
 			if (body != null) {
 				contentType(ContentType.Application.Json)
@@ -40,5 +42,39 @@ class Client(private val hostUrl: String) {
 		body: Any? = null,
 		block: HttpRequestBuilder.() -> Unit = {}
 	) = request<Out>(HttpMethod.Post, url, body, block)
+
+	internal suspend inline fun <reified Out> get(
+		url: String,
+		body: Any? = null,
+		block: HttpRequestBuilder.() -> Unit = {}
+	) = request<Out>(HttpMethod.Get, url, body, block)
+
+}
+
+/**
+ * A client without credentials.
+ */
+class AnonymousClient(hostUrl: String) : Client(hostUrl) {
+
+	override val client = HttpClient {
+		install(JsonFeature) {
+			serializer = KotlinxSerializer()
+		}
+
+		install(Logging)
+	}
+
+}
+
+/**
+ * A client connected by a [token].
+ * To get a token, use [formulaide.client.routes.login].
+ */
+class AuthenticatedClient(private val _client: Client, private val token: String) : Client(_client.hostUrl) {
+
+	override val client: HttpClient
+		get() = _client.client
+
+	//TODO: authenticate
 
 }
