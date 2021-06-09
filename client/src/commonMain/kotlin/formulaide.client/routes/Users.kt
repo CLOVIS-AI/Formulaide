9c26948feacc1956a92ae5922d4934955b3719fb
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-09 10:09:51 +0200

    feat: Implemented /users/create and /users/me

--- client/src/commonMain/kotlin/formulaide.client/routes/Users.kt
+++ client/src/commonMain/kotlin/formulaide.client/routes/Users.kt
@@ -1,7 +1,10 @@
 package formulaide.client.routes
 
+import formulaide.api.users.NewUser
 import formulaide.api.users.PasswordLogin
 import formulaide.api.users.TokenResponse
+import formulaide.api.users.User
+import formulaide.client.AuthenticatedClient
 import formulaide.client.Client
 
 /**
@@ -13,3 +16,24 @@ import formulaide.client.Client
  */
 suspend fun Client.login(passwordLogin: PasswordLogin) =
 	post<TokenResponse>("/users/login", body = passwordLogin)
+
+/**
+ * Creates a new user.
+ *
+ * - POST /users/create
+ * - Requires 'administrator' right
+ * - Body: [NewUser]
+ * - Response: [TokenResponse]
+ */
+suspend fun AuthenticatedClient.createUser(newUser: NewUser) =
+	post<TokenResponse>("/users/create", body = newUser)
+
+/**
+ * Gets the current user's data.
+ *
+ * - GET /users/me
+ * - Requires authentication
+ * - Response: [User]
+ */
+suspend fun AuthenticatedClient.getMe() =
+	get<User>("/users/me")
--- server/src/main/kotlin/formulaide/server/routes/UserRouting.kt
+++ server/src/main/kotlin/formulaide/server/routes/UserRouting.kt
@@ -1,9 +1,16 @@
 package formulaide.server.routes
 
+import formulaide.api.users.NewUser
 import formulaide.api.users.PasswordLogin
 import formulaide.api.users.TokenResponse
+import formulaide.db.document.toApi
 import formulaide.server.Auth
+import formulaide.server.Auth.Companion.Employee
+import formulaide.server.Auth.Companion.requireAdmin
+import formulaide.server.Auth.Companion.requireEmployee
+import formulaide.server.database
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -18,5 +25,21 @@ fun Routing.userRoutes(auth: Auth) {
 			call.respond(TokenResponse(token))
 		}
 
+		authenticate(Employee) {
+			post("/create") {
+				call.requireAdmin(database)
+				val data = call.receive<NewUser>()
+
+				val (token, _) = auth.newAccount(data)
+
+				call.respond(TokenResponse(token))
+			}
+
+			get("/me") {
+				val user = call.requireEmployee(database)
+
+				call.respond(user.toApi())
+			}
+		}
 	}
 }
