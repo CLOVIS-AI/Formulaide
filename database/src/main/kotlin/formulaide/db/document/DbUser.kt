a61cd917a826ae86489aa6d51da8ea403d656380
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-09 10:22:25 +0200

    feat(database): Implemented findUserById

--- database/src/main/kotlin/formulaide/db/document/DbUser.kt
+++ database/src/main/kotlin/formulaide/db/document/DbUser.kt
@@ -32,11 +32,18 @@ suspend fun Database.findUser(email: String): DbUser? =
 	users.findOne(DbUser::email eq email)
 
 /**
+ * Finds a user in the database, by searching for an exact match with its [id].
+ */
+suspend fun Database.findUserById(id: DbUserId): DbUser? =
+	users.findOne(DbUser::id eq id)
+
+/**
  * Creates a [user], and returns it.
  */
 suspend fun Database.createUser(user: DbUser): DbUser {
 	checkNotNull(findService(user.service)) { "Le service ${user.service} n'existe pas" }
 	check(findUser(user.email) == null) { "Un utilisateur avec cette adresse mail existe déjà" }
+	check(findUserById(user.id) == null) { "Un utilisateur avec cet identifiant existe déjà" }
 
 	users.insertOne(user)
 
