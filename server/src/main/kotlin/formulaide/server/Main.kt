3b44dd2a66b3bec1793d9f7db1cad36119c362e7
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-10 17:53:24 +0200

    feat(server): Implemented routes for compound data creation

--- server/src/main/kotlin/formulaide/server/Main.kt
+++ server/src/main/kotlin/formulaide/server/Main.kt
@@ -5,6 +5,7 @@ import formulaide.api.users.User
 import formulaide.db.Database
 import formulaide.db.document.createService
 import formulaide.server.Auth.Companion.Employee
+import formulaide.server.routes.dataRoutes
 import formulaide.server.routes.serviceRoutes
 import formulaide.server.routes.userRoutes
 import io.ktor.application.*
@@ -63,5 +64,6 @@ fun Application.formulaide(@Suppress("UNUSED_PARAMETER") testing: Boolean = fals
 	routing {
 		userRoutes(auth)
 		serviceRoutes()
+		dataRoutes()
 	}
 }
