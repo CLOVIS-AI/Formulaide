5e2285cee94342397737ee85794950206fe33f0d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-10 10:40:48 +0200

    feat(server): Added endpoint /services/create

--- server/src/main/kotlin/formulaide/server/routes/ServiceRouting.kt
+++ server/src/main/kotlin/formulaide/server/routes/ServiceRouting.kt
@@ -1,15 +1,13 @@
 package formulaide.server.routes
 
-import formulaide.db.document.DbService
-import formulaide.db.document.allServices
-import formulaide.db.document.allServicesIgnoreOpen
-import formulaide.db.document.toApi
+import formulaide.db.document.*
 import formulaide.server.Auth.Companion.Employee
 import formulaide.server.Auth.Companion.requireAdmin
 import formulaide.server.Auth.Companion.requireEmployee
 import formulaide.server.database
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -33,6 +31,15 @@ fun Routing.serviceRoutes() {
 					.map(DbService::toApi)
 
 				call.respond(services)
+			}
+
+			post("/create") {
+				call.requireAdmin(database)
+
+				val service = call.receive<String>()
+				val created = database.createService(service)
+
+				call.respond(created)
 			}
 		}
 
