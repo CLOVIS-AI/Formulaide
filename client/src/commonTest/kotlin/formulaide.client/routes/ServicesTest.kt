051d1f3ac9d91ae006f6c64ef9f5068de5a27f0d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-10 11:07:39 +0200

    tests(client): Added service access/edition tests

---
+++ client/src/commonTest/kotlin/formulaide.client/routes/ServicesTest.kt
@@ -0,0 +1,49 @@
+package formulaide.client.routes
+
+import formulaide.client.runTest
+import formulaide.client.testAdministrator
+import formulaide.client.testEmployee
+import kotlin.test.Test
+import kotlin.test.assertFails
+import kotlin.test.assertTrue
+
+class ServicesTest {
+
+	@Test
+	fun list() = runTest {
+		val client = testEmployee()
+
+		val services = client.listServices()
+
+		assertTrue(services.isNotEmpty())
+		assertTrue(services.all { it.open })
+	}
+
+	@Test
+	fun fullList() = runTest {
+		val client = testAdministrator()
+
+		val services = client.listAllServices()
+
+		assertTrue(services.isNotEmpty())
+	}
+
+	@Test
+	fun createAsEmployee() = runTest {
+		val client = testEmployee()
+
+		assertFails {
+			client.createService("Un super service qui ne peut pas être créé")
+		}
+	}
+
+	@Test
+	fun createAsAdmin() = runTest {
+		val client = testAdministrator()
+
+		val service = client.createService("Un super service qui peut être créé")
+
+		assertTrue(service.open)
+	}
+
+}
