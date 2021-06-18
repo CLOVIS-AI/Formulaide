c6433eed85c615d8c57d596c6e1c59b5080a8b21
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-18 15:24:03 +0200

    feat(ui): Very simple navigation scheme

--- ui/src/main/kotlin/formulaide.ui/App.kt
+++ ui/src/main/kotlin/formulaide.ui/App.kt
@@ -15,11 +15,17 @@ import react.functionalComponent
 import react.useMemo
 import react.useState
 
+enum class Screen {
+	HOME,
+}
+
 /**
  * The main app screen.
  */
 val App = functionalComponent<RProps> {
 	val defaultClient = useMemo { Client.Anonymous.connect("http://localhost:8000") } //TODO: generify
+
+	val (screen, setScreen) = useState(Screen.HOME)
 
 	val (client, setClient) = useState<Client>(defaultClient)
 	val (user, setUser) = useState<User?>(null)
@@ -32,21 +38,23 @@ val App = functionalComponent<RProps> {
 		}
 	}
 
-	div {
-		if (user == null) {
-			login {
-				this.client = client
-				this.onLogin = { client, user ->
-					setClient(client)
-					setUser(user)
+	when (screen) {
+		Screen.HOME -> div {
+			if (user == null) {
+				login {
+					this.client = client
+					this.onLogin = { client, user ->
+						setClient(client)
+						setUser(user)
+					}
+					this.scope = scope
 				}
-				this.scope = scope
-			}
-		} else {
-			button {
-				text("Se déconnecter")
-				attrs {
-					onClickFunction = { setClient(defaultClient); setUser(null) }
+			} else {
+				button {
+					text("Se déconnecter")
+					attrs {
+						onClickFunction = { setClient(defaultClient); setUser(null); setScreen(Screen.HOME) }
+					}
 				}
 			}
 		}
