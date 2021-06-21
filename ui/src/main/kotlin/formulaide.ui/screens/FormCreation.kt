b6f063ae0ae43d6eaf8cf832f8787a75c15ffe2b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-21 16:27:09 +0200

    feat(ui): Select actions when creating a form

--- ui/src/main/kotlin/formulaide.ui/screens/FormCreation.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/FormCreation.kt
@@ -3,18 +3,22 @@ package formulaide.ui.screens
 import formulaide.api.data.*
 import formulaide.api.data.Data.Simple.SimpleDataId.TEXT
 import formulaide.api.types.Arity
+import formulaide.api.users.Service
 import formulaide.client.Client
 import formulaide.client.routes.createForm
 import formulaide.client.routes.listData
+import formulaide.client.routes.listServices
 import formulaide.ui.fields.editableField
 import formulaide.ui.utils.text
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.html.InputType
 import kotlinx.html.id
+import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.onSubmitFunction
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLSelectElement
 import react.*
 import react.dom.*
 
@@ -68,14 +72,22 @@ val CreateForm = functionalComponent<FormCreationProps> { props ->
 				this.arity = field.arity
 				this.data = field.data
 				this.compounds = compoundData
-				this.set = {  name, data, min, max, subFields ->
+				this.set = { name, data, min, max, subFields ->
 					val newName = name ?: field.name
 					val newData = data ?: field.data
 					val newMin = min ?: field.arity.min
 					val newMax = max ?: field.arity.max
 					val newFields = subFields ?: field.components
 
-					replaceField(i, field.copy(name = newName, data = newData, arity = Arity(newMin, newMax), components = newFields))
+					replaceField(
+						i,
+						field.copy(
+							name = newName,
+							data = newData,
+							arity = Arity(newMin, newMax),
+							components = newFields
+						)
+					)
 				}
 
 				this.recursive = true
@@ -103,6 +115,96 @@ val CreateForm = functionalComponent<FormCreationProps> { props ->
 		}
 
 		val (actions, setActions) = useState<List<Action>>(emptyList())
+		fun replaceAction(index: Int, value: Action) =
+			setActions(actions.subList(0, index) + value + actions.subList(index + 1, actions.size))
+
+		val (services, setServices) = useState(emptyList<Service>())
+		useEffect(listOf(props.client)) {
+			props.scope.launch {
+				val client = props.client
+				require(client is Client.Authenticated) { "Impossible de récupérer la liste des services avec un client non connecté" }
+
+				setServices(client.listServices())
+			}
+		}
+
+		for ((i, action) in actions.withIndex()) {
+			br {}
+			label { text("Action $i") }
+			select {
+				option {
+					text("Vérification par un service")
+
+					attrs {
+						value = "SERVICE_REVIEW"
+						selected = action is Action.ServiceReview
+					}
+				}
+				option {
+					text("Vérification par un employé")
+
+					attrs {
+						value = "EMPLOYEE_REVIEW"
+						selected = action is Action.EmployeeReview
+					}
+				}
+
+				attrs {
+					onChangeFunction = {
+						when ((it.target as HTMLSelectElement).value) {
+							"SERVICE_REVIEW" -> Action.ServiceReview(action.id, action.id, services.getOrNull(0) ?: error("Aucun service n'a été trouvé"))
+							"EMPLOYEE_REVIEW" -> TODO("La responsabilité d'un employé n'est pas encore implémentée")
+						}
+					}
+				}
+			}
+
+			when (action) {
+				is Action.ServiceReview -> {
+					br {}
+					label { text("Choix du service") }
+					select {
+						for (service in services) {
+							option {
+								text(service.name)
+
+								attrs {
+									value = service.id.toString()
+									selected = action.service == service.id
+								}
+							}
+						}
+
+						attrs {
+							onChangeFunction = { event ->
+								val serviceId = (event.target as HTMLSelectElement).value.toInt()
+								val service = services.find { it.id == serviceId } ?: error("Impossible de trouver le service '$serviceId'")
+
+								replaceAction(i, Action.ServiceReview(action.id, action.order, service))
+							}
+						}
+					}
+				}
+				is Action.EmployeeReview -> TODO("La responsabilité d'un employé n'est pas encore implémentée")
+			}
+		}
+
+		br {}
+		input(InputType.button, name = "new-data-add-action") {
+			attrs {
+				value = "Ajouter une action"
+				onClickFunction = {
+					setActions(
+						actions + Action.ServiceReview(
+							id = actions.size,
+							order = actions.size,
+							service = services.getOrNull(0) ?: error("Aucun service n'a été trouvé")
+						)
+					)
+				}
+			}
+		}
+
 
 		br {}
 		input(InputType.submit, name = "new-form-submit") {
@@ -119,7 +221,8 @@ val CreateForm = functionalComponent<FormCreationProps> { props ->
 				val form = Form(
 					name = formName.current?.value ?: error("Le formulaire n'a pas de nom"),
 					id = 0,
-					public = public.current?.checked ?: error("Le formulaire ne précise pas s'il est public ou interne"),
+					public = public.current?.checked
+						?: error("Le formulaire ne précise pas s'il est public ou interne"),
 					open = true,
 					fields = fields,
 					actions = actions
