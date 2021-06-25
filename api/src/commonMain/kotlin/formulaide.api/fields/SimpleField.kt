2815cf91b159c644793895e1d3e3116bd478583f
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-25 15:46:11 +0200

    feat(api): Added missing simple types

--- api/src/commonMain/kotlin/formulaide.api/fields/SimpleField.kt
+++ api/src/commonMain/kotlin/formulaide.api/fields/SimpleField.kt
@@ -1,33 +1,65 @@
 package formulaide.api.fields
 
+import formulaide.api.fields.SimpleField.Message.arity
 import formulaide.api.types.Arity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * A field that represents some specific data.
+ *
+ * This class does not implement [Field], because it is meant to be wrapped inside a [Field] implementation:
+ * see [DataField.Simple], [FormField.Simple] and [DeepFormField.Simple].
+ * For this reason, this class doesn't have an [id][Field.id] nor a [name][Field.name].
+ */
 @Serializable
 sealed class SimpleField {
-	abstract val name: String
 	abstract val arity: Arity
 
+	/**
+	 * The user should input some text.
+	 * @property maxLength The maximum number of characters allowed (`null` means 'no limit').
+	 */
 	@SerialName("TEXT")
 	data class Text(
-		override val name: String,
 		override val arity: Arity,
 		val maxLength: Int? = null,
 	) : SimpleField()
 
+	/**
+	 * The user should input an integer.
+	 * @property min The minimum value of that integer (`null` means 'no minimum').
+	 * @property max The maximum value of that integer (`null` means 'no maximum').
+	 */
 	@SerialName("INTEGER")
 	data class Integer(
-		override val name: String,
 		override val arity: Arity,
 		val min: Int? = null,
 		val max: Int? = null,
 	) : SimpleField()
 
+	/**
+	 * The user should input a decimal number.
+	 */
+	@SerialName("INTEGER")
+	data class Decimal(
+		override val arity: Arity,
+	) : SimpleField()
+
+	/**
+	 * The user should check a box.
+	 */
+	@SerialName("INTEGER")
+	data class Boolean(
+		override val arity: Arity,
+	) : SimpleField()
+
+	/**
+	 * A message should be displayed to the user, but they shouldn't have anything to fill in.
+	 * The [arity] is always [Arity.mandatory].
+	 */
 	@SerialName("MESSAGE")
-	data class Message(
-		override val name: String,
-	) : SimpleField() {
+	object Message: SimpleField() {
 		override val arity get() = Arity.mandatory()
 	}
 
