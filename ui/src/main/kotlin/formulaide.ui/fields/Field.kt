0edda2dd9eebc4ad8e28b118bdc3379a6955c370
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-29 15:12:59 +0200

    refactor(ui): Rewrote Field to use the new API

--- ui/src/main/kotlin/formulaide.ui/fields/Field.kt
+++ ui/src/main/kotlin/formulaide.ui/fields/Field.kt
@@ -1,75 +1,71 @@
 package formulaide.ui.fields
 
-import formulaide.api.data.*
-import formulaide.api.data.Data.Simple.SimpleDataId.*
+import formulaide.api.fields.FormField
+import formulaide.api.fields.SimpleField
 import formulaide.api.types.Arity
 import formulaide.ui.ScreenProps
 import formulaide.ui.utils.text
 import kotlinx.html.INPUT
 import kotlinx.html.InputType
+import kotlinx.html.js.onChangeFunction
+import org.w3c.dom.HTMLInputElement
 import react.*
 import react.dom.*
 
 private external interface FieldProps : RProps {
 	var app: ScreenProps
+	var field: FormField
 
 	var id: String
-
-	var name: String
-	var arity: Arity
-	var type: Data
-
-	var formField: AbstractFormField
-	var compoundField: CompoundDataField?
 }
 
 private val Field: FunctionalComponent<FieldProps> = functionalComponent { props ->
+	val field = props.field
+
 	br {}
 	label {
-		text(props.name)
+		text(field.name)
 
-		if (props.arity == Arity.mandatory())
+		if (field.arity == Arity.mandatory())
 			text("*")
 	}
 
 	val configureName: RDOMBuilder<INPUT>.() -> Unit = {
 		attrs {
-			println("Configuring field with name ${props.id}")
 			name = props.id
 		}
 	}
 
 	val configureRequired: RDOMBuilder<INPUT>.() -> Unit = {
 		attrs {
-			required = props.arity == Arity.mandatory()
+			required = field.arity == Arity.mandatory()
 		}
 	}
 
-	when (val type = props.type) {
-		is Data.Simple -> when (type.id) { //TODO: check validity of value
-			TEXT -> input(InputType.text) {
+	when (field) {
+		is FormField.Simple -> when (field.simple) { //TODO: check validity of value
+			is SimpleField.Text -> input(InputType.text) {
 				configureName()
 				configureRequired()
 			}
-			INTEGER -> input(InputType.number) {
+			is SimpleField.Integer -> input(InputType.number) {
 				configureName()
 				configureRequired()
 			}
-			MESSAGE -> Unit // Nothing to do, see MESSAGE
-			DECIMAL -> input(InputType.number) {
+			is SimpleField.Message -> Unit // Nothing to do, see MESSAGE
+			is SimpleField.Decimal -> input(InputType.number) {
 				configureName()
 				configureRequired()
 				attrs {
 					step = "any"
 				}
 			}
-			BOOLEAN -> input(InputType.checkBox) {
+			is SimpleField.Boolean -> input(InputType.checkBox) {
 				configureName()
 			}
 		}
-		is Data.Compound -> {
-			val compound = props.app.compounds.find { it.id == type.id }
-			requireNotNull(compound) { "L'identifiant '${type.id}' ne correspond à aucune donnée connue" }
+		is FormField.Composite -> {
+			val subFields = field.fields
 
 			div {
 				attrs {
@@ -78,55 +74,55 @@ private val Field: FunctionalComponent<FieldProps> = functionalComponent { props
 					}
 				}
 
-				for (formField in props.formField.components!!) {
-					val compoundField = compound.fields.find { it.id == formField.id }
-					requireNotNull(compoundField) { "Le champ du formulaire ${formField.id} ne correspond à aucun champ de la donnée '${compound.id}'" }
-
-					deepField(props.app, formField, compoundField, props.id)
+				for (subField in subFields) {
+					field(props.app, subField, "${props.id}:${subField.id}")
 				}
 			}
 		}
-		is Data.Union -> Unit //TODO: unions
+		is FormField.Union<*> -> {
+			val subFields = field.options
+			val (selected, setSelected) = useState(subFields.first())
+
+			div {
+				attrs {
+					jsStyle {
+						marginLeft = "2rem"
+					}
+				}
+
+				for (subField in subFields) {
+					input(InputType.radio, name = "${props.id}:${subField.id}") {
+						attrs {
+							value = field.id
+							checked = subField == selected
+
+							onChangeFunction = { event ->
+								setSelected(subFields.first { it.id == (event.target as HTMLInputElement).value })
+							}
+						}
+					}
+					label {
+						text(subField.name)
+						attrs { htmlFor = "${props.id}:${subField.id}" }
+					}
+				}
+
+				field(props.app, selected, "${props.id}:${selected.id}")
+			}
+		}
 	}
 
 	//TODO: lists
 }
 
-fun RBuilder.topLevelField(
-	screenProps: ScreenProps,
+fun RBuilder.field(
+	app: ScreenProps,
 	field: FormField,
-	parentId: String?,
+	id: String? = null,
 ) = child(Field) {
 	attrs {
-		app = screenProps
-
-		id = (parentId?.plus(":") ?: "") + field.id.toString()
-
-		name = field.name
-		arity = field.arity
-		type = field.data
-
-		formField = field
-		compoundField = null
-	}
-}
-
-fun RBuilder.deepField(
-	screenProps: ScreenProps,
-	field: FormFieldComponent,
-	compoundField: CompoundDataField,
-	parentId: String?
-) = child(Field) {
-	attrs {
-		app = screenProps
-
-		id = (parentId?.plus(":") ?: "") + field.id.toString()
-
-		name = compoundField.name
-		arity = field.arity
-		type = compoundField.data
-
-		formField = field
-		this.compoundField = compoundField
+		this.app = app
+		this.field = field
+		this.id = id ?: field.id
 	}
 }
