f521ecf2abac283d9074bb7023ca66f99ff996e3
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-29 14:10:47 +0200

    feat(ui): The field editor can now add sub-fields to a recursive field

--- ui/src/main/kotlin/formulaide.ui/fields/RecursionEditor.kt
+++ ui/src/main/kotlin/formulaide.ui/fields/RecursionEditor.kt
@@ -3,11 +3,15 @@ package formulaide.ui.fields
 import formulaide.api.fields.DataField
 import formulaide.api.fields.Field
 import formulaide.api.fields.FormField
+import formulaide.api.fields.SimpleField
+import formulaide.api.types.Arity
 import formulaide.ui.utils.replace
 import formulaide.ui.utils.text
+import kotlinx.html.js.onChangeFunction
 import react.FunctionalComponent
 import react.child
 import react.dom.attrs
+import react.dom.button
 import react.dom.div
 import react.dom.jsStyle
 import react.functionalComponent
@@ -35,12 +39,41 @@ val RecursionEditor: FunctionalComponent<FieldProps2> = functionalComponent { pr
 						this.field = field
 						replace = { newField ->
 							val newParent = when (parent) {
-								is DataField.Union -> parent.copy(options = parent.options.replace(i, newField as DataField))
-								is FormField.Shallow.Union -> parent.copy(options = parent.options.replace(i, newField as FormField.Shallow))
-								is FormField.Deep.Union -> parent.copy(options = parent.options.replace(i, newField as FormField.Deep))
-								is FormField.Shallow.Composite -> parent.copy(fields = parent.fields.replace(i, newField as FormField.Deep))
-								is FormField.Deep.Composite -> parent.copy(fields = parent.fields.replace(i, newField as FormField.Deep))
+								is DataField.Union -> parent.copy(options = parent.options
+									.replace(i, newField as DataField))
+								is FormField.Shallow.Union -> parent.copy(options = parent.options
+									.replace(i, newField as FormField.Shallow))
+								is FormField.Deep.Union -> parent.copy(options = parent.options
+									.replace(i, newField as FormField.Deep))
+								is FormField.Shallow.Composite -> parent.copy(fields = parent.fields
+									.replace(i, newField as FormField.Deep))
+								is FormField.Deep.Composite -> parent.copy(fields = parent.fields
+									.replace(i, newField as FormField.Deep))
 								else -> error("Impossible de modifier les sous-champs de $parent")
+							}
+
+							props.replace(newParent)
+						}
+					}
+				}
+			}
+
+			if (parent is DataField.Union || parent is DataField.Composite || parent is FormField.Shallow.Union) {
+				button {
+					text("Ajouter un champ")
+					attrs {
+						onChangeFunction = {
+							val id = fields.size.toString()
+							val order = fields.size
+							val name = "Nouveau champ"
+							val simple = SimpleField.Text(Arity.optional())
+
+							val newParent = when (parent) {
+								is DataField.Union -> parent.copy(options = parent.options +
+										DataField.Simple(id, order, name, simple))
+								is FormField.Shallow.Union -> parent.copy(options = parent.options +
+										FormField.Shallow.Simple(id, order, name, simple))
+								else -> error("Impossible d'ajouter un sous-champ à $parent")
 							}
 
 							props.replace(newParent)
