f6f63f9d621dc91616ec7273f8b908bc64318c69
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-06-30 17:24:50 +0200

    feat(ui): Use the default Tailwind configuration in the UI

--- ui/src/main/kotlin/formulaide.ui/Main.kt
+++ ui/src/main/kotlin/formulaide.ui/Main.kt
@@ -1,6 +1,7 @@
 package formulaide.ui
 
 import formulaide.ui.utils.detectTests
+import kotlinext.js.require
 import kotlinx.browser.document
 import react.child
 import react.dom.render
@@ -8,6 +9,8 @@ import react.dom.render
 fun main() {
 	if (detectTests())
 		return
+
+	require("./styles.css")
 
 	render(document.getElementById("root")) {
 		child(App) {}
