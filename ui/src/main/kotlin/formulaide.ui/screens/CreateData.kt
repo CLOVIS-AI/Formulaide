e765638c86212e63c1aba829da2bd73ae484588e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-07-02 18:05:24 +0200

    refactor(ui): Styled data creation

--- ui/src/main/kotlin/formulaide.ui/screens/CreateData.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/CreateData.kt
@@ -8,18 +8,17 @@ import formulaide.api.types.Ref
 import formulaide.client.Client
 import formulaide.client.routes.createData
 import formulaide.ui.ScreenProps
-import formulaide.ui.components.styledCard
+import formulaide.ui.components.*
 import formulaide.ui.fields.FieldEditor
 import formulaide.ui.utils.replace
 import formulaide.ui.utils.text
 import kotlinx.coroutines.launch
 import kotlinx.html.InputType
-import kotlinx.html.id
-import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.onSubmitFunction
 import org.w3c.dom.HTMLInputElement
 import react.child
-import react.dom.*
+import react.dom.li
+import react.dom.ul
 import react.functionalComponent
 import react.useRef
 import react.useState
@@ -40,81 +39,61 @@ val CreateData = functionalComponent<ScreenProps> { props ->
 		}
 	}
 
-	p { text("Une donnée composée permet de combiner plusieurs données possédant un lien (par exemple, le numéro de téléphone et l'adresse mail).") }
-	p { text("Les données composées sont stockées et unifiées entre les services.") }
-
 	val formName = useRef<HTMLInputElement>()
 	val (fields, setFields) = useState<List<DataField>>(emptyList())
 
-	div {
-		text("Créer une donnée :")
-		form {
-			div {
-				label { text("Nom") }
-				input(InputType.text, name = "new-data-name") {
-					attrs {
-						id = "new-data-name"
-						required = true
-						autoFocus = true
-						placeholder = "Nom de la donnée"
-						ref = formName
-					}
+	styledFormCard(
+		"Créer une donnée",
+		"Une donnée composée permet de combiner plusieurs données possédant un lien " +
+				"(par exemple, le numéro de téléphone et l'adresse mail). " +
+				"Les données composées sont stockées et unifiées entre les services.",
+		"Créer cette donnée",
+		"Ajouter un champ" to {
+			setFields(
+				fields + DataField.Simple(
+					order = fields.size,
+					id = fields.size.toString(),
+					name = "Nouveau champ",
+					simple = SimpleField.Text(Arity.optional())
+				)
+			)
+		},
+		contents = {
+			styledField("new-data-name", "Nom") {
+				styledInput(InputType.text, "new-data-name", required = true, ref = formName) {
+					autoFocus = true
 				}
 			}
 
-			for ((i, field) in fields.withIndex()) {
-				br {}
-				child(FieldEditor) {
-					attrs {
-						app = props
-						this.field = field
-						replace = {
-							setFields(fields.replace(i, it as DataField))
+			styledField("data-fields", "Champs") {
+				styledNesting {
+					for ((i, field) in fields.withIndex()) {
+						child(FieldEditor) {
+							attrs {
+								app = props
+								this.field = field
+								replace = {
+									setFields(fields.replace(i, it as DataField))
+								}
+							}
 						}
 					}
 				}
 			}
+		},
+	) {
+		onSubmitFunction = {
+			it.preventDefault()
 
-			br {}
-			input(InputType.button, name = "new-data-add-simple") {
-				attrs {
-					value = "Ajouter un champ"
-					onClickFunction = {
-						setFields(
-							fields + DataField.Simple(
-								order = fields.size,
-								id = fields.size.toString(),
-								name = "Nouveau champ",
-								simple = SimpleField.Text(Arity.optional())
-							)
-						)
-					}
-				}
-			}
+			val data = Composite(
+				id = Ref.SPECIAL_TOKEN_NEW,
+				name = formName.current?.value ?: error("Cette donnée n'a pas de nom"),
+				fields = fields
+			)
 
-			br {}
-			input(InputType.submit, name = "new-data-submit") {
-				attrs {
-					id = "new-data-submit"
-					value = "Créer cette donnée"
-				}
-			}
-
-			attrs {
-				onSubmitFunction = {
-					it.preventDefault()
-
-					val data = Composite(
-						id = Ref.SPECIAL_TOKEN_NEW,
-						name = formName.current?.value ?: error("Cette donnée n'a pas de nom"),
-						fields = fields
-					)
-
-					props.scope.launch {
-						client.createData(data)
-						props.refreshComposites()
-					}
-				}
+			props.scope.launch {
+				client.createData(data)
+				props.refreshComposites()
 			}
 		}
 	}
