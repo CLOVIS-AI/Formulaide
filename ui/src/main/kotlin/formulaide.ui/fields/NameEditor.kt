67278d0f2612c6b44c0bcf68b17e2a05361f875c
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-07-02 16:08:36 +0200

    refactor(ui): Field names now follow the design

--- ui/src/main/kotlin/formulaide.ui/fields/NameEditor.kt
+++ ui/src/main/kotlin/formulaide.ui/fields/NameEditor.kt
@@ -2,32 +2,29 @@ package formulaide.ui.fields
 
 import formulaide.api.fields.DataField
 import formulaide.api.fields.FormField
+import formulaide.ui.components.styledField
+import formulaide.ui.components.styledInput
 import formulaide.ui.utils.text
 import kotlinx.html.InputType
 import kotlinx.html.js.onChangeFunction
 import org.w3c.dom.HTMLInputElement
-import react.dom.attrs
-import react.dom.input
-import react.dom.label
 import react.functionalComponent
 
 val NameEditor = functionalComponent<EditableFieldProps> { props ->
 	val field = props.field
 	val allowModification = field is DataField || field is FormField.Shallow
 
-	if (allowModification) {
-		label { text("Nom") }
-		input(InputType.text, name = "item-name") {
-			attrs {
-				required = true
+	styledField("item-name-${field.id}", "Nom") {
+		if (allowModification) {
+			styledInput(InputType.text, "item-name-${field.id}", required = true) {
 				placeholder = "Nom du champ"
 				onChangeFunction = {
 					props.replace(
 						props.field.set(name = (it.target as HTMLInputElement).value))
 				}
 			}
+		} else {
+			text(" ${props.field.name}")
 		}
-	} else {
-		text(" ${props.field.name}")
 	}
 }
