d3f221031b0cd758026e0e4543bfb1ef66a1f46d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-07-05 16:03:27 +0200

    style(ui): Migrate the list of forms to its own card

--- ui/src/main/kotlin/formulaide.ui/screens/ListForms.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/ListForms.kt
@@ -2,32 +2,24 @@ package formulaide.ui.screens
 
 import formulaide.ui.Screen
 import formulaide.ui.ScreenProps
-import formulaide.ui.utils.text
-import kotlinx.html.js.onClickFunction
-import react.dom.attrs
-import react.dom.button
-import react.dom.li
-import react.dom.ul
+import formulaide.ui.components.styledButton
+import formulaide.ui.components.styledCard
+import formulaide.ui.components.styledField
 import react.functionalComponent
 
 val FormList = functionalComponent<ScreenProps> { props ->
 	val forms = props.forms
 
-	ul {
-		for (form in forms) {
-			var text = form.name
-
-			if (!form.public)
-				text += " • Ce formulaire est interne"
-
-			li { text(text) }
-			button {
-				text("Saisir")
-				attrs {
-					onClickFunction = { props.navigateTo(Screen.SubmitForm(form)) }
+	styledCard(
+		"Formulaires",
+		null,
+		contents = {
+			for (form in forms) {
+				styledField("form-${form.id}", form.name) {
+					styledButton("Remplir") { props.navigateTo(Screen.SubmitForm(form)) }
 				}
 			}
 		}
-	}
+	)
 
 }
