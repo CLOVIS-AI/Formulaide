f717c0c70e5304c8c5738963706915f3511d33d1
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-07-06 16:44:21 +0200

    style(ui): Nicer error message for server failures

--- ui/src/main/kotlin/formulaide.ui/Errors.kt
+++ ui/src/main/kotlin/formulaide.ui/Errors.kt
@@ -2,8 +2,12 @@ package formulaide.ui
 
 import formulaide.ui.components.styledCard
 import formulaide.ui.utils.text
+import io.ktor.client.call.*
+import io.ktor.client.features.*
 import kotlinx.coroutines.launch
 import react.functionalComponent
+import react.useEffect
+import react.useState
 
 external interface ErrorProps : ScreenProps {
 	var error: Throwable
@@ -12,13 +16,35 @@ external interface ErrorProps : ScreenProps {
 val ErrorCard = functionalComponent<ErrorProps> { props ->
 	val error = props.error
 
-	styledCard(
-		error.message ?: error.toString(),
-		error::class.simpleName,
-		failed = true
-	) {
-		text("Here, in the future: an error message")
+	var title by useState(error.message ?: error.toString())
+	var subtitle by useState(error::class.simpleName ?: error::class.toString())
+	var body by useState<String>()
+
+	useEffect(error) {
+		props.scope.launch {
+			when (error) {
+				is ServerResponseException -> {
+					val responseText = error.response.receive<String>()
+
+					if (responseText.isBlank()) {
+						title = error.response.status.toString()
+					} else {
+						title = responseText
+						subtitle = "${error.response.status} • $subtitle"
+					}
+
+					body = "Le serveur a refusé la connexion."
+				}
+			}
+		}
 	}
+
+	styledCard(
+		title,
+		subtitle,
+		"OK" to { },
+		failed = true,
+	) { body?.let { text(it) } }
 }
 
 inline fun reportExceptions(reporter: (Throwable) -> Unit, block: () -> Unit) {
