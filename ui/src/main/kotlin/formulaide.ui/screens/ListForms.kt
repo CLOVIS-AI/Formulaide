2ca16bfa9dc0335c5af1bd53f9155166e9dc4d31
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-07-27 16:36:56 +0200

    feat(ui): Display the number of records in each state, in the form list

--- ui/src/main/kotlin/formulaide.ui/screens/ListForms.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/ListForms.kt
@@ -1,17 +1,20 @@
 package formulaide.ui.screens
 
 import formulaide.api.data.Form
+import formulaide.api.data.Record
+import formulaide.api.data.RecordState
 import formulaide.api.types.Ref.Companion.createRef
-import formulaide.ui.Screen
+import formulaide.client.Client
+import formulaide.client.routes.todoListFor
+import formulaide.ui.*
 import formulaide.ui.components.styledButton
 import formulaide.ui.components.styledCard
-import formulaide.ui.components.styledField
-import formulaide.ui.traceRenders
-import formulaide.ui.useForms
-import formulaide.ui.useNavigation
-import react.RProps
-import react.child
-import react.fc
+import formulaide.ui.components.styledNesting
+import formulaide.ui.components.useAsync
+import formulaide.ui.utils.text
+import react.*
+import react.dom.div
+import react.dom.p
 
 val FormList = fc<RProps> { _ ->
 	traceRenders("FormList")
@@ -40,10 +43,82 @@ internal external interface FormDescriptionProps : RProps {
 
 internal val FormDescription = fc<FormDescriptionProps> { props ->
 	val form = props.form
-
 	val (_, navigateTo) = useNavigation()
 
-	styledField("form-${form.id}", form.name) {
-		styledButton("Remplir") { navigateTo(Screen.SubmitForm(form.createRef())) }
+	p { text(form.name) }
+	styledNesting(depth = 0) {
+
+		div {
+			text("Actions : ")
+
+			styledButton("Remplir") { navigateTo(Screen.SubmitForm(form.createRef())) }
+		}
+
+		div {
+			text("Dossiers : ")
+
+			for (action in form.actions.sortedBy { it.order }) {
+				child(ActionDescription) {
+					attrs {
+						this.form = form
+						this.state = RecordState.Action(action.createRef())
+					}
+				}
+			}
+
+			child(ActionDescription) {
+				attrs {
+					this.form = form
+					this.state = RecordState.Done
+				}
+			}
+
+			child(ActionDescription) {
+				attrs {
+					this.form = form
+					this.state = RecordState.Refused
+				}
+			}
+		}
+	}
+}
+
+internal external interface ActionDescriptionProps : RProps {
+	var form: Form
+	var state: RecordState
+}
+
+internal val ActionDescription = fc<ActionDescriptionProps> { props ->
+	val form = props.form
+	val state = props.state
+
+	val (client) = useClient()
+	val user by useUser()
+	val scope = useAsync()
+
+	var records by useState(emptyList<Record>())
+	useEffect(client, user) {
+		if (user != null && client is Client.Authenticated)
+			scope.reportExceptions {
+				records = client.todoListFor(form, state)
+			}
+	}
+
+	val stateName = when (state) {
+		//TODO: replace by the action's name after #109
+		is RecordState.Action -> state.current.obj.id.takeIf { user?.service?.id == state.current.obj.reviewer.id }
+		is RecordState.Done -> "Acceptés"
+		is RecordState.Refused -> "Refusés"
+	}
+	if (stateName != null) {
+		val title = stateName +
+				when {
+					records.size == 1 -> " (1 dossier)"
+					records.size == Record.MAXIMUM_NUMBER_OF_RECORDS_PER_ACTION -> " (${Record.Companion.MAXIMUM_NUMBER_OF_RECORDS_PER_ACTION}+ dossiers)"
+					records.isNotEmpty() -> " (${records.size} dossiers)"
+					else -> ""
+				}
+
+		styledButton(title, action = {})
 	}
 }
