449884e1b9b4fdec80839478e4c18c9e63cdf793
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-08-16 13:42:14 +0200

    tests(client): Upload a real file

--- client/src/jvmTest/kotlin/formulaide/client/files/MultipartTestJVM.kt
+++ client/src/jvmTest/kotlin/formulaide/client/files/MultipartTestJVM.kt
@@ -3,13 +3,30 @@ package formulaide.client.files
 import formulaide.client.runTest
 import org.junit.Test
 import java.io.File
+import kotlin.test.assertFails
 
 class MultipartTestJVM {
 
 	@Test
-	fun multipartTestJvm() = runTest {
-		@Suppress("BlockingMethodInNonBlockingContext")
-		multipartTest(FileUploadJVM(File.createTempFile("formulaide-multipart-test-jvm", ".txt")))
+	fun emptyJvm() = runTest {
+		assertFails {
+			@Suppress("BlockingMethodInNonBlockingContext")
+			multipartTest(FileUploadJVM(File.createTempFile("formulaide-multipart-test-jvm",
+			                                                ".txt")))
+		}
+	}
+
+	@Test
+	fun imageJvm() = runTest {
+		val resource = MultipartTestJVM::class.java
+			.getResource("/images/book-flowers.jpg")
+			?: error("Could not find resource")
+
+		val file = resource
+			.path
+			.let { File(it) }
+
+		multipartTest(FileUploadJVM(file))
 	}
 
 }
