1cbdb019b77e81f307050fa7844a389d8c084af1
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-08-23 12:24:05 +0200

    feat(ui): Search pills now display the field name

--- ui/src/main/kotlin/formulaide.ui/screens/Review.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/Review.kt
@@ -1,6 +1,7 @@
 package formulaide.ui.screens
 
 import formulaide.api.data.*
+import formulaide.api.fields.FormField
 import formulaide.api.fields.FormRoot
 import formulaide.api.search.SearchCriterion
 import formulaide.api.types.Ref.Companion.createRef
@@ -169,7 +170,6 @@ internal fun Review(form: Form, state: RecordState, initialRecords: List<Record>
 				for (criterion in criteria)
 					child(CriterionPill) {
 						attrs {
-							key = criterion.fieldKey + "-" + criterion.hashCode()
 							this.root = root
 							this.fields = root?.fields ?: form.mainFields
 							this.criterion = criterion
@@ -219,13 +219,42 @@ private external interface CriterionPillProps : RProps {
 private val CriterionPill = memo(fc<CriterionPillProps> { props ->
 	var showFull by useState(false)
 
+	val fields = useMemo(props.fields, props.criterion.fieldKey) {
+		val fieldKeys = props.criterion.fieldKey.split(":")
+		val fields = ArrayList<FormField>(fieldKeys.size)
+
+		fields.add(props.fields.fields.find { it.id == fieldKeys[0] }
+			           ?: error("Aucun champ n'a l'ID ${fieldKeys[0]}"))
+
+		for ((i, key) in fieldKeys.withIndex().drop(1)) {
+			fields.add(when (val current = fields[i - 1]) {
+				           is FormField.Simple -> error("Impossible d'obtenir un enfant d'un champ simple")
+				           is FormField.Union<*> -> current.options.find { it.id == key }
+					           ?: error("Aucune option n'a l'ID $key: ${current.options}")
+				           is FormField.Composite -> current.fields.find { it.id == key }
+					           ?: error("Aucun champ n'a l'ID $key: ${current.fields}")
+			           })
+		}
+
+		fields
+	}
+
 	styledPill {
 		styledButton(
 			if (showFull) "‹"
 			else "›",
 			action = { showFull = !showFull }
 		)
-		text(props.criterion.fieldKey)
+		if (showFull) {
+			text(props.root?.name ?: "Saisie originelle")
+
+			for (field in fields) {
+				br {}
+				text("→ ${field.name}")
+			}
+		} else {
+			text(fields.last().name)
+		}
 		text(" ")
 		text(when (val criterion = props.criterion) {
 			     is SearchCriterion.Exists -> "a été rempli"
