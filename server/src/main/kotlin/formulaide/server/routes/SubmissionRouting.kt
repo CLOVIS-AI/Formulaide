2e3bf4705dc9de63901ac58e1fd41b8f434d7cc3
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-08-25 19:07:53 +0200

    feat(server): Accept regular HTML forms

--- server/src/main/kotlin/formulaide/server/routes/SubmissionRouting.kt
+++ server/src/main/kotlin/formulaide/server/routes/SubmissionRouting.kt
@@ -3,18 +3,21 @@ package formulaide.server.routes
 import formulaide.api.data.FormSubmission
 import formulaide.api.data.RecordsToReviewRequest
 import formulaide.api.data.ReviewRequest
+import formulaide.api.types.Ref
+import formulaide.api.types.Ref.Companion.createRef
 import formulaide.db.document.*
 import formulaide.server.Auth
 import formulaide.server.Auth.Companion.requireEmployee
 import formulaide.server.database
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Routing.submissionRoutes() {
-	route("submissions") {
+	route("/submissions") {
 
 		post("/create") {
 			val submission = call.receive<FormSubmission>()
@@ -25,6 +28,39 @@ fun Routing.submissionRoutes() {
 			database.createRecord(dbSubmission.toApi())
 
 			call.respond("Success")
+		}
+
+		post("/nativeCreate/{formId}") {
+			val formId =
+				call.parameters["formId"] ?: error("Le paramètre GET 'formId' est obligatoire.")
+			val form = database.findForm(formId)?.takeIf { it.public }
+				?: error("Le formulaire demandé n'existe pas, ou n'est pas public : $formId")
+			require(form.open) { "Le formulaire demandé a été archivé, il n'est plus possible d'y répondre." }
+
+			val data = HashMap<String, String>()
+
+			call.receiveMultipart().forEachPart {
+				when (it) {
+					is PartData.FormItem -> {
+						data[it.name ?: error("La valeur '${it.value}' devrait avoir un nom.")] =
+							it.value
+					}
+					else -> error("Le type de données '${it::class}' n'est pas supporté.")
+				}
+
+				it.dispose()
+			}
+
+			val submission = FormSubmission(
+				id = Ref.SPECIAL_TOKEN_NEW,
+				form = form.createRef(),
+				root = null, // The raw HTML can only be used for the original submission
+				data = data,
+			)
+			val dbSubmission = database.saveSubmission(submission)
+			database.createRecord(dbSubmission.toApi())
+
+			call.respondText("SUCCESS")
 		}
 
 		authenticate(Auth.Employee) {
