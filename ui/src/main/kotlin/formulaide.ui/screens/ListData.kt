1db5fc86925e40ead9467fa667a67cc2d0231015
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-07 10:41:11 +0200

    feat(ui): Archive/reopen composites in the composite list

--- ui/src/main/kotlin/formulaide.ui/screens/ListData.kt
+++ ui/src/main/kotlin/formulaide.ui/screens/ListData.kt
@@ -1,27 +1,58 @@
 package formulaide.ui.screens
 
+import formulaide.api.data.CompositeMetadata
+import formulaide.client.Client
+import formulaide.client.routes.editData
 import formulaide.ui.*
 import formulaide.ui.Role.Companion.role
 import formulaide.ui.components.styledButton
 import formulaide.ui.components.styledCard
+import formulaide.ui.components.styledCheckbox
 import formulaide.ui.components.styledField
+import kotlinx.html.js.onChangeFunction
+import org.w3c.dom.HTMLInputElement
 import react.RProps
 import react.fc
+import react.useState
 
 val DataList = fc<RProps> {
 	traceRenders("DataList")
 
+	val (client) = useClient()
 	val user by useUser()
-	val composites by useComposites()
+	val composites by useAllComposites()
+
+	var showArchived by useState(false)
 
 	styledCard(
 		"Groupes",
 		null,
 		contents = {
-			for (composite in composites) {
+			if (user.role >= Role.EMPLOYEE) styledField("hide-disabled", "Groupes archivés") {
+				styledCheckbox("hide-disabled", "Afficher les groupes archivés") {
+					onChangeFunction =
+						{ showArchived = (it.target as HTMLInputElement).checked }
+				}
+			}
+
+			for (composite in composites.filter { showArchived || it.open }) {
 				styledField("composite-${composite.id}", composite.name) {
-					if (user.role >= Role.ADMINISTRATOR)
+					if (user.role >= Role.ADMINISTRATOR) {
+						require(client is Client.Authenticated) { "Le client devrait être connecté." }
+
 						styledButton("Copier", action = { navigateTo(Screen.NewData(composite)) })
+
+						styledButton(
+							if (composite.open) "Archiver" else "Désarchiver",
+							action = {
+								client.editData(CompositeMetadata(
+									composite.id,
+									!composite.open,
+								))
+								refreshComposites()
+							}
+						)
+					}
 				}
 			}
 		}
