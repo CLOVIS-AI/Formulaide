7a980a894b357fc862d1d38751a738f898a9e9d5
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-15 12:38:00 +0200

    feat(ui): Give the GitLab Service Desk email address for bug reports

--- ui/src/main/kotlin/formulaide.ui/App.kt
+++ ui/src/main/kotlin/formulaide.ui/App.kt
@@ -194,7 +194,7 @@ internal class CrashReporter : RComponent<RProps, CrashReporterState>() {
 				"Le site a rencontré un échec fatal",
 				failed = true,
 			) {
-				p { text("Veuillez signaler cette erreur à l'administrateur, en lui envoyant les informations ci-dessous :") }
+				p { text("Veuillez signaler cette erreur à l'administrateur, en lui envoyant les informations ci-dessous, à l'adresse incoming+clovis-ai-formulaide-27107472-issue-@incoming.gitlab.com :") }
 
 				p(errorSectionClass) {
 					text("Ce que j'étais en train de faire : ")
