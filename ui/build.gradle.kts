22149e53687dbfa89ada91668e744719c67eed31
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-17 14:27:08 +0200

    upgrade: React use-error-boundary 2.0.6

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation(kotlinWrapper("react-dom", Version.kotlinReact))
 	implementation(npm("react", Version.react))
 	implementation(npm("react-dom", Version.react))
+	implementation(npm("use-error-boundary", Version.reactUseErrorBoundary))
 
 	implementation(npm("postcss", Version.postcss))
 	implementation(npm("postcss-loader", Version.postcssLoader))
