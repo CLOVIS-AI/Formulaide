f51d222433d8b1c74fde46dae14328661ca619e8
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-11-13 12:53:31 +0100

    upgrade: PostCSS 8.3.11, PostCSS-Loader 6.2.0

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
 	implementation(npm("react-dom", "17.0.2"))
 	implementation(npm("use-error-boundary", "2.0.6"))
 
-	implementation(npm("postcss", "8.3.9"))
-	implementation(npm("postcss-loader", "6.1.1"))
+	implementation(npm("postcss", "8.3.11"))
+	implementation(npm("postcss-loader", "6.2.0"))
 	implementation(npm("autoprefixer", "10.3.7"))
 	implementation(npm("tailwindcss", "2.2.16"))
 
