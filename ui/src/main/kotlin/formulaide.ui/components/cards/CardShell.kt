c45d8038eefc3e3e14632a5e0abc1ef3e545dc31
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-02-04 14:45:30 +0100

    feat(ui): Support for headers and footers in CardShell
    This is the first step in implementing https://gitlab.com/arcachon-ville/formulaide/-/issues/254

--- ui/src/main/kotlin/formulaide.ui/components/cards/CardShell.kt
+++ ui/src/main/kotlin/formulaide.ui/components/cards/CardShell.kt
@@ -1,5 +1,6 @@
 package formulaide.ui.components.cards
 
+import react.ChildrenBuilder
 import react.FC
 import react.PropsWithChildren
 import react.dom.html.ReactHTML.div
@@ -8,6 +9,9 @@ external interface CardShellProps : PropsWithChildren {
 	var id: String?
 	var failed: Boolean?
 	var mini: Boolean?
+
+	var header: ((ChildrenBuilder) -> Unit)?
+	var footer: ((ChildrenBuilder) -> Unit)?
 }
 
 val CardShell = FC<CardShellProps>("CardShell") { props ->
@@ -16,6 +20,29 @@ val CardShell = FC<CardShellProps>("CardShell") { props ->
 		className = "m-4 px-8 shadow-lg rounded-lg z-10 relative bg-white" +
 				(if (props.mini == true) " py-4" else " py-8") +
 				(if (props.failed == true) " bg-red-200" else "")
+
+		props.header?.let { header ->
+			div {
+				header(this)
+			}
+		}
+
 		props.children()
+
+		props.footer?.let { footer ->
+			div {
+				footer(this)
+			}
+		}
 	}
+}
+
+@Suppress("FunctionName") // This function looks like a component
+fun CardShellProps.Header(block: ChildrenBuilder.() -> Unit) {
+	header = block
+}
+
+@Suppress("FunctionName") // This function looks like a component
+fun CardShellProps.Footer(block: ChildrenBuilder.() -> Unit) {
+	footer = block
 }
