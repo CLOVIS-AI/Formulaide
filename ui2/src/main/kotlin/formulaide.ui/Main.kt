7072049a00e6fcca76396514fdfee3da0b00d842
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-06-30 18:36:15 +0200

    feat(ui2): Load the correct page using the URL
    See https://gitlab.com/opensavvy/formulaide/-/issues/77

--- ui2/src/main/kotlin/formulaide.ui/Main.kt
+++ ui2/src/main/kotlin/formulaide.ui/Main.kt
@@ -1,9 +1,12 @@
 package formulaide.ui
 
 import formulaide.ui.navigation.Navigation
+import formulaide.ui.navigation.loadNavigation
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
+	loadNavigation()
+
 	renderComposable(rootElementId = "root") {
 		Navigation()
 	}
--- ui2/src/main/kotlin/formulaide.ui/navigation/Navigation.kt
+++ ui2/src/main/kotlin/formulaide.ui/navigation/Navigation.kt
@@ -4,10 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import formulaide.ui.screens.DummyScreen
 import formulaide.ui.screens.Home
 import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.url.URL
 
+@Suppress("ObjectPropertyName")
 private var _currentScreen by mutableStateOf(Home)
 
 var currentScreen: Screen
@@ -18,7 +21,28 @@ var currentScreen: Screen
 		_currentScreen = value
 	}
 
+val screens = listOf(
+	Home,
+	DummyScreen,
+)
+
 @Composable
 fun Navigation() {
 	_currentScreen()
+}
+
+fun loadNavigation() {
+	val url = URL(window.location.href)
+		.search
+
+	val params = url.split("&")
+	console.log("Loading the navigation system... Requested route: $params")
+
+	val target = params.getOrNull(0) ?: Home.route
+	val screen = screens
+		.firstOrNull { it.route.startsWith(target) }
+	console.log("...it refers to the screen $screen")
+
+	if (screen != null)
+		currentScreen = screen
 }
