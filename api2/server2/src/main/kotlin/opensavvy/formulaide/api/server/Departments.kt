c42ca5516918149d6173987854ab3594e152fb39
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-10-20 14:55:37 +0200

    feat(api2): Authorization verification for the department API

--- api2/server2/src/main/kotlin/opensavvy/formulaide/api/server/Departments.kt
+++ api2/server2/src/main/kotlin/opensavvy/formulaide/api/server/Departments.kt
@@ -3,19 +3,24 @@ package opensavvy.formulaide.api.server
 import io.ktor.server.routing.*
 import kotlinx.coroutines.flow.emitAll
 import opensavvy.backbone.Ref.Companion.request
+import opensavvy.formulaide.api.Context
 import opensavvy.formulaide.api.Department
 import opensavvy.formulaide.api.api2
 import opensavvy.formulaide.api.utils.bind
 import opensavvy.formulaide.api.utils.mapSuccess
 import opensavvy.formulaide.api.utils.onEachSuccess
 import opensavvy.formulaide.database.Database
+import opensavvy.spine.ktor.server.ContextGenerator
 import opensavvy.spine.ktor.server.route
 import opensavvy.formulaide.core.Department as CoreDepartment
 
-fun Routing.departments(database: Database) {
+fun Routing.departments(database: Database, contextGenerator: ContextGenerator<Context>) {
 
-	route(api2.departments.get, context) {
-		//TODO rights management
+	route(api2.departments.get, contextGenerator) {
+		ensureEmployee { "Seuls les employés peuvent accéder à la liste des départements" }
+
+		if (parameters.includeClosed)
+			ensureAdministrator { "Seuls les administrateurs peuvent accéder aux départements fermés" }
 
 		val result = database.departments.list(parameters.includeClosed)
 			.mapSuccess { list ->
@@ -25,22 +30,23 @@ fun Routing.departments(database: Database) {
 		emitAll(result)
 	}
 
-	route(api2.departments.id.get, context) {
-		//TODO rights management
+	route(api2.departments.id.get, contextGenerator) {
+		ensureEmployee { "Seuls les employés peuvent accéder aux départements" }
 
 		val id = api2.departments.id.idFrom(id, context).let { bind(it) }
 		val result = CoreDepartment.Ref(id, database.departments)
 			.request()
 			.onEachSuccess {
-				//TODO rights management: check that it is open
+				if (!it.open)
+					ensureAdministrator { "Seuls les administrateurs peuvent accéder aux départements fermés" }
 			}
 			.mapSuccess { Department(name = it.name, open = it.open) }
 
 		emitAll(result)
 	}
 
-	route(api2.departments.create, context) {
-		//TODO rights management
+	route(api2.departments.create, contextGenerator) {
+		ensureAdministrator { "Seuls les administrateurs peuvent créer des départements" }
 
 		val result = database.departments.create(body.name)
 			.mapSuccess { api2.departments.id.idOf(it.id) to Unit }
@@ -48,8 +54,8 @@ fun Routing.departments(database: Database) {
 		emitAll(result)
 	}
 
-	route(api2.departments.id.visibility, context) {
-		//TODO rights management
+	route(api2.departments.id.visibility, contextGenerator) {
+		ensureAdministrator { "Seuls les administrateurs peuvent modifier le statut d'un département" }
 
 		val id = api2.departments.id.idFrom(id, context).let { bind(it) }
 		val ref = CoreDepartment.Ref(id, database.departments)
