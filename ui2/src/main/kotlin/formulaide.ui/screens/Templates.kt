aaa4e47a0d2312679683c5cce2be913cc9ecaead
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-10-27 14:52:27 +0200

    feat(ui2): List of templates

--- ui2/src/main/kotlin/formulaide.ui/screens/Templates.kt
+++ ui2/src/main/kotlin/formulaide.ui/screens/Templates.kt
@@ -1,8 +1,16 @@
 package formulaide.ui.screens
 
-import formulaide.ui.components.Page
+import androidx.compose.runtime.*
+import formulaide.ui.components.*
 import formulaide.ui.navigation.Screen
+import formulaide.ui.navigation.client
+import formulaide.ui.utils.rememberRef
+import formulaide.ui.utils.rememberState
+import opensavvy.formulaide.core.Template
 import opensavvy.formulaide.core.User
+import opensavvy.state.Slice.Companion.valueOrNull
+import opensavvy.state.firstResultOrNull
+import org.jetbrains.compose.web.dom.Text
 
 val TemplateList: Screen = Screen(
 	title = "Modèles",
@@ -11,9 +19,57 @@ val TemplateList: Screen = Screen(
 	icon = "ri-clipboard-line",
 	iconSelected = "ri-clipboard-fill"
 ) {
+	var showArchived by remember { mutableStateOf(false) }
+	var forceRefresh by remember { mutableStateOf(0) }
+
+	val templates by rememberState(
+		client,
+		forceRefresh,
+		showArchived
+	) { client.templates.list(includeClosed = showArchived) }
+
 	Page(
-		"Modèles"
+		title = "Modèles",
+		header = {
+			ChipContainerContainer {
+				ChipContainer {
+					FilterChip("Archivés", showArchived, onUpdate = { showArchived = it })
+				}
+
+				ChipContainer {
+					RefreshButton {
+						client.templates.cache.expireAll()
+						forceRefresh++
+					}
+				}
+			}
+
+			DisplayError(templates)
+		}
 	) {
-		//TODO in future commits of this MR
+		for (template in templates.valueOrNull ?: emptyList()) {
+			ShowTemplate(template)
+		}
+	}
+}
+
+@Composable
+private fun ShowTemplate(ref: Template.Ref) {
+	val slice by rememberRef(ref)
+	val template = slice.valueOrNull
+
+	Paragraph(template?.name ?: "", loading = slice.progression) {
+		Text("${template?.versions?.size ?: "1"} versions")
+
+		ButtonContainer {
+			TextButton(
+				{ client.templates.edit(ref, open = !template!!.open).firstResultOrNull() },
+				enabled = template != null
+			) {
+				Text(if (template?.open != false) "Archiver" else "Désarchiver")
+			}
+		}
+
+		DisplayError(slice)
 	}
 }
