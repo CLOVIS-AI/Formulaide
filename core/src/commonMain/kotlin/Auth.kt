e8d87e72736d8d93cd68fd8963bce70fd1c97cae
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-12-23 22:19:55 +0100

    feat(core): User API

---
+++ core/src/commonMain/kotlin/Auth.kt
@@ -0,0 +1,107 @@
+package opensavvy.formulaide.core
+
+import arrow.core.continuations.EffectScope
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.withContext
+import opensavvy.formulaide.core.Auth.Companion.currentAuth
+import opensavvy.formulaide.core.Auth.Companion.currentRole
+import opensavvy.formulaide.core.Auth.Companion.currentUser
+import opensavvy.state.Failure
+import opensavvy.state.slice.ensureAuthenticated
+import opensavvy.state.slice.ensureAuthorized
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Authentication information for the current user.
+ *
+ * This information is passed implicitly within the [CoroutineContext] to all functions in the application.
+ * Because of this, authentication is only available in `suspend` functions.
+ *
+ * To authenticate the current function call, use [withContext]:
+ * ```
+ * withContext(Auth(User.Role.Employee, theirReference)) {
+ *     // Inside this block of code, the user is authenticated as an Employee
+ * }
+ * ```
+ *
+ * To access the authentication information in your code, the accessor [currentAuth] is provided.
+ */
+class Auth(
+	/**
+	 * The role of the current user.
+	 *
+	 * @see currentRole
+	 */
+	val role: User.Role,
+
+	/**
+	 * The current user.
+	 *
+	 * @see currentUser
+	 */
+	val user: User.Ref?,
+) : AbstractCoroutineContextElement(Key) {
+
+	init {
+		if (role >= User.Role.Employee)
+			requireNotNull(user) { "Erreur interne : vous êtes authentifié comme un employé ou administrateur, mais votre identifiant d'utilisateur est manquant" }
+	}
+
+	object Key : CoroutineContext.Key<Auth>
+
+	companion object {
+
+		//region Accessors
+
+		/**
+		 * Accesses the authentication information for the current scope.
+		 *
+		 * @see Auth
+		 */
+		suspend fun currentAuth(): Auth? =
+			currentCoroutineContext()[Key]
+
+		/**
+		 * Accesses the current user.
+		 *
+		 * @see Auth
+		 * @see user
+		 */
+		suspend fun currentUser(): User.Ref? =
+			currentAuth()?.user
+
+		/**
+		 * Accesses the role of the current user.
+		 *
+		 * If the current session is not authenticated, [User.Role.Anonymous] is returned.
+		 *
+		 * @see Auth
+		 * @see role
+		 */
+		suspend fun currentRole(): User.Role =
+			currentAuth()?.role ?: User.Role.Anonymous
+
+		//endregion
+
+		/**
+		 * Checks that the user has at least the role [User.Role.Employee].
+		 */
+		suspend fun EffectScope<Failure>.ensureEmployee(
+			lazyMessage: () -> String = { "Seuls les employés ont accès à cette ressource" },
+		) {
+			ensureAuthenticated(currentRole() >= User.Role.Employee, lazyMessage)
+		}
+
+		/**
+		 * Checks that the user has at least the role [User.Role.Administrator].
+		 */
+		suspend fun EffectScope<Failure>.ensureAdministrator(
+			lazyMessage: () -> String = { "Seuls les administrateurs ont accès à cette ressource" },
+		) {
+			ensureEmployee(lazyMessage)
+			ensureAuthorized(currentRole() >= User.Role.Administrator, lazyMessage)
+		}
+
+	}
+}
---
+++ core/src/commonMain/kotlin/User.kt
@@ -0,0 +1,271 @@
+package opensavvy.formulaide.core
+
+import opensavvy.backbone.Backbone
+import opensavvy.formulaide.core.data.Email
+import opensavvy.formulaide.core.data.Password
+import opensavvy.formulaide.core.data.Token
+import opensavvy.state.slice.Slice
+import kotlin.jvm.JvmName
+
+/**
+ * A user of the project.
+ *
+ * Formulaide is built so end-users do not require accounts.
+ * This class therefore only represents real users of the application.
+ *
+ * Only administrators can create new accounts.
+ */
+data class User(
+	/**
+	 * The email address of this user.
+	 */
+	val email: Email,
+
+	/**
+	 * The name of this user.
+	 */
+	val name: String,
+
+	/**
+	 * Whether this user is active.
+	 *
+	 * It is not possible to log in as an inactive user.
+	 * Only administrators can see inactive users.
+	 */
+	val active: Boolean,
+
+	/**
+	 * The role of this user.
+	 */
+	val administrator: Boolean,
+
+	/**
+	 * The departments this user is a part of.
+	 *
+	 * The user can see submissions to forms and templates assigned to any of these departments.
+	 */
+	val departments: Set<Department.Ref>,
+
+	/**
+	 * If `true`, this user is currently limited to a single-use password.
+	 *
+	 * After authenticating themselves, they will be forced to select a new password.
+	 */
+	val singleUsePassword: Boolean,
+) {
+
+	enum class Role {
+		Anonymous,
+		Employee,
+		Administrator,
+		;
+
+		companion object {
+
+			val User.role
+				get() = when (administrator) {
+					true -> Administrator
+					false -> Employee
+				}
+
+			@get:JvmName("getRoleNullable")
+			val User?.role get() = this?.role ?: Anonymous
+		}
+	}
+
+	data class Ref(
+		val id: String,
+		override val backbone: Service,
+	) : opensavvy.backbone.Ref<User> {
+
+		/**
+		 * Joins [department].
+		 *
+		 * @see User.departments
+		 * @see Service.join
+		 */
+		suspend fun join(department: Department.Ref) = backbone.join(this, department)
+
+		/**
+		 * Leaves [department].
+		 *
+		 * @see User.departments
+		 * @see Service.leave
+		 */
+		suspend fun leave(department: Department.Ref) = backbone.leave(this, department)
+
+		/**
+		 * Enables this user.
+		 *
+		 * @see User.active
+		 * @see Service.edit
+		 */
+		suspend fun enable() = backbone.edit(this, active = true)
+
+		/**
+		 * Disables this user.
+		 *
+		 * @see User.active
+		 * @see Service.edit
+		 */
+		suspend fun disable() = backbone.edit(this, active = false)
+
+		/**
+		 * Promotes this user to [Role.Administrator].
+		 *
+		 * @see User.administrator
+		 * @see Service.edit
+		 */
+		suspend fun promote() = backbone.edit(this, administrator = true)
+
+		/**
+		 * Demotes this user to [Role.Employee].
+		 *
+		 * @see User.administrator
+		 * @see Service.edit
+		 */
+		suspend fun demote() = backbone.edit(this, administrator = false)
+
+		/**
+		 * Resets this user's password.
+		 *
+		 * @see User.singleUsePassword
+		 * @see Service.resetPassword
+		 */
+		suspend fun resetPassword() = backbone.resetPassword(this)
+
+		/**
+		 * Sets this user's password.
+		 *
+		 * @see User.singleUsePassword
+		 * @see Service.setPassword
+		 */
+		suspend fun setPassword(oldPassword: String, newPassword: Password) =
+			backbone.setPassword(this, oldPassword, newPassword)
+
+		/**
+		 * Verifies this [token].
+		 *
+		 * @see Service.verifyToken
+		 */
+		suspend fun verifyToken(token: Token) = backbone.verifyToken(this, token)
+
+		/**
+		 * Invalidates this [token].
+		 */
+		suspend fun logOut(token: Token) = backbone.logOut(this, token)
+
+		override fun toString() = "Utilisateur $id"
+	}
+
+	interface Service : Backbone<User> {
+
+		/**
+		 * Lists all users.
+		 *
+		 * Requires administrator authentication.
+		 */
+		suspend fun list(includeClosed: Boolean = false): Slice<List<Ref>>
+
+		/**
+		 * Creates a new user.
+		 *
+		 * Requires administrator authentication.
+		 *
+		 * @return A reference to the created user, as well as their generated single-use password.
+		 */
+		suspend fun create(
+			email: Email,
+			fullName: String,
+			administrator: Boolean = false,
+		): Slice<Pair<Ref, Password>>
+
+		/**
+		 * Adds [department] to the list of departments [user] is a part of.
+		 *
+		 * Requires administrator authentication.
+		 */
+		suspend fun join(
+			user: Ref,
+			department: Department.Ref,
+		): Slice<Unit>
+
+		/**
+		 * Removes [department] from the list of departments [user] is a part of.
+		 *
+		 * Requires administrator authentication.
+		 */
+		suspend fun leave(
+			user: Ref,
+			department: Department.Ref,
+		): Slice<Unit>
+
+		/**
+		 * Edits [user].
+		 *
+		 * Requires administrator authentication.
+		 */
+		suspend fun edit(
+			user: Ref,
+			active: Boolean? = null,
+			administrator: Boolean? = null,
+		): Slice<Unit>
+
+		//region Account management
+
+		/**
+		 * Resets [user]'s password.
+		 *
+		 * The user will be disconnected from their account on all their devices.
+		 *
+		 * Requires administrator authentication.
+		 *
+		 * @return A new single-use password.
+		 */
+		suspend fun resetPassword(
+			user: Ref,
+		): Slice<Password>
+
+		/**
+		 * Sets [user]'s password.
+		 *
+		 * A user may only set their own password.
+		 */
+		suspend fun setPassword(
+			user: Ref,
+			oldPassword: String,
+			newPassword: Password,
+		): Slice<Unit>
+
+		//endregion
+		//region Authentication
+
+		/**
+		 * Checks that [token] is a valid token for [user].
+		 */
+		suspend fun verifyToken(
+			user: Ref,
+			token: Token,
+		): Slice<Unit>
+
+		/**
+		 * Checks that [email] and [password] match an existing user.
+		 *
+		 * @return The reference to the logged-in user, and a valid connection token.
+		 */
+		suspend fun logIn(
+			email: Email,
+			password: Password,
+		): Slice<Pair<Ref, Token>>
+
+		/**
+		 * Invalidates [token] for [user].
+		 */
+		suspend fun logOut(
+			user: Ref,
+			token: Token,
+		): Slice<Unit>
+
+		//endregion
+	}
+}
