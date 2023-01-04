e1e43480d99c91c2d7a9c6d4b71795f3a9163c37
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-04 22:18:49 +0100

    refactor(remote): Convenience function to convert Ktor authentication to Auth

---
+++ remote-server/src/main/kotlin/utils/Authenticated.kt
@@ -0,0 +1,16 @@
+package opensavvy.formulaide.server.utils
+
+import io.ktor.server.auth.*
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.withContext
+import opensavvy.formulaide.core.Auth
+import opensavvy.formulaide.server.AuthPrincipal
+import opensavvy.spine.ktor.server.ResponseStateBuilder
+
+suspend fun <R> ResponseStateBuilder<*, *, *>.authenticated(block: suspend () -> R): R {
+	val auth = call.principal<AuthPrincipal>()?.auth ?: Auth.Anonymous
+
+	return withContext(
+		auth + CoroutineName("${auth.user?.id}(${auth.role})")
+	) { block() }
+}
