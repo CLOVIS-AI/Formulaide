0536a544b64f5c993a46bdee9569e15ef4b86f6d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-09 23:52:29 +0100

    test: TestScheduler-aware clock implementation

---
+++ test/src/commonMain/kotlin/utils/TestClock.kt
@@ -0,0 +1,22 @@
+package opensavvy.formulaide.test.utils
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlin.coroutines.coroutineContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestClock(private val scheduler: TestCoroutineScheduler) : Clock {
+	override fun now(): Instant = Instant.fromEpochMilliseconds(scheduler.currentTime)
+
+	companion object {
+		suspend fun testClock(): TestClock {
+			val scheduler = coroutineContext[TestCoroutineScheduler]
+				?: error("No TestCoroutineScheduler is installed in this context: $coroutineContext")
+			return TestClock(scheduler)
+		}
+
+		suspend fun currentInstant() = testClock().now()
+	}
+}
