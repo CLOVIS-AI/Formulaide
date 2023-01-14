1b236fcd3062b7a4098b6ccc7f81d296a6f946b3
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-14 14:46:41 +0100

    feat(backend): Initial Ktor setup

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -8,9 +8,12 @@ dependencies {
 	implementation(projects.remoteServer)
 
 	implementation(Ktor.server)
+	implementation(Ktor.server.netty)
 	implementation(Ktor.plugins.serialization)
 
 	implementation(KotlinX.serialization.core)
+
+	implementation("ch.qos.logback:logback-classic:_")
 }
 
 application {
--- backend/src/main/kotlin/Backend.kt
+++ backend/src/main/kotlin/Backend.kt
@@ -1,5 +1,30 @@
 package opensavvy.formulaide.backend
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import opensavvy.formulaide.server.configureServer
+
 fun main() {
-	println("Hello world!")
+	embeddedServer(
+		Netty,
+		port = 9000,
+		module = Application::formulaide,
+	).start(wait = true)
+}
+
+fun Application.formulaide() {
+	configureServer()
+
+	if (developmentMode) {
+		log.error("DEVELOPMENT MODE IS ENABLED. THIS IS NOT SAFE FOR PRODUCTION.")
+	}
+
+	routing {
+		get {
+			call.respondText("Hello world!")
+		}
+	}
 }
