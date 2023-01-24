d4cc0c76983724582d8fb66497f285afc9c67316
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-24 23:27:16 +0100

    tests(remote): Test authentication using CoroutineContext

--- remote-server/src/main/kotlin/Form.kt
+++ remote-server/src/main/kotlin/Form.kt
@@ -1,6 +1,5 @@
 package opensavvy.formulaide.remote.server
 
-import io.ktor.server.auth.*
 import io.ktor.server.routing.*
 import opensavvy.backbone.Ref.Companion.request
 import opensavvy.formulaide.core.Department
@@ -9,7 +8,6 @@ import opensavvy.formulaide.core.Template
 import opensavvy.formulaide.remote.api
 import opensavvy.formulaide.remote.dto.SchemaDto.Companion.toDto
 import opensavvy.formulaide.remote.dto.SchemaDto.Companion.toForm
-import opensavvy.formulaide.remote.server.utils.authenticated
 import opensavvy.spine.Identified
 import opensavvy.spine.ktor.server.route
 import opensavvy.state.progressive.firstValue
@@ -18,83 +16,71 @@ fun Routing.forms(
 	departments: Department.Service,
 	templates: Template.Service,
 	forms: Form.Service,
-) = authenticate(optional = true) {
+) {
 
 	route(api.forms.get, contextGenerator) {
-		authenticated {
-			forms.list(includeClosed = parameters.includeClosed)
-				.bind()
-				.map { api.templates.id.idOf(it.id) }
-		}
+		forms.list(includeClosed = parameters.includeClosed)
+			.bind()
+			.map { api.templates.id.idOf(it.id) }
 	}
 
 	route(api.forms.create, contextGenerator) {
-		authenticated {
-			forms.create(
-				body.name,
-				body.firstVersion.toForm(
-					decodeDepartment = {
-						api.departments.id.refOf(it, departments).bind()
-					},
-					decodeTemplate = {
-						api.templates.id.version.refOf(it, templates).bind()
-					}
-				)
-			).bind()
-				.let { api.templates.id.idOf(it.id) }
-				.let { Identified(it, Unit) }
-		}
+		forms.create(
+			body.name,
+			body.firstVersion.toForm(
+				decodeDepartment = {
+					api.departments.id.refOf(it, departments).bind()
+				},
+				decodeTemplate = {
+					api.templates.id.version.refOf(it, templates).bind()
+				}
+			)
+		).bind()
+			.let { api.templates.id.idOf(it.id) }
+			.let { Identified(it, Unit) }
 	}
 
 	route(api.forms.id.create, contextGenerator) {
-		authenticated {
-			val ref = api.forms.id.refOf(id, forms).bind()
+		val ref = api.forms.id.refOf(id, forms).bind()
 
-			forms.createVersion(
-				ref,
-				body.toForm(
-					decodeDepartment = {
-						api.departments.id.refOf(it, departments).bind()
-					},
-					decodeTemplate = {
-						api.templates.id.version.refOf(it, templates).bind()
-					}
-				)
-			).bind()
-				.let { api.forms.id.version.idOf(it.form.id, it.version.toString()) }
-				.let { Identified(it, Unit) }
-		}
+		forms.createVersion(
+			ref,
+			body.toForm(
+				decodeDepartment = {
+					api.departments.id.refOf(it, departments).bind()
+				},
+				decodeTemplate = {
+					api.templates.id.version.refOf(it, templates).bind()
+				}
+			)
+		).bind()
+			.let { api.forms.id.version.idOf(it.form.id, it.version.toString()) }
+			.let { Identified(it, Unit) }
 	}
 
 	route(api.forms.id.edit, contextGenerator) {
-		authenticated {
-			val ref = api.forms.id.refOf(id, forms).bind()
+		val ref = api.forms.id.refOf(id, forms).bind()
 
-			forms.edit(
-				ref,
-				name = body.name,
-				open = body.open,
-				public = body.public,
-			).bind()
-		}
+		forms.edit(
+			ref,
+			name = body.name,
+			open = body.open,
+			public = body.public,
+		).bind()
 	}
 
 	route(api.forms.id.get, contextGenerator) {
-		authenticated {
-			val ref = api.forms.id.refOf(id, forms).bind()
+		val ref = api.forms.id.refOf(id, forms).bind()
 
-			ref.request().firstValue().bind()
-				.toDto()
-		}
+		ref.request().firstValue().bind()
+			.toDto()
 	}
 
 	route(api.forms.id.version.get, contextGenerator) {
-		authenticated {
-			val version = api.forms.id.version.refOf(id, forms).bind()
+		val version = api.forms.id.version.refOf(id, forms).bind()
 
-			version.request().firstValue().bind()
-				.toDto()
-		}
+		version.request().firstValue().bind()
+			.toDto()
 	}
 
 }
