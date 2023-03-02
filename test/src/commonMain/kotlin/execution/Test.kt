b45ec88e95fa7956ecad34b5ac58253b8f0ca670
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-03-02 22:56:52 +0100

    tests(test): Created a Test coroutine element that stores data and executes finalizers

--- test/src/commonMain/kotlin/execution/Test.kt
+++ test/src/commonMain/kotlin/execution/Test.kt
@@ -1,6 +1,9 @@
 package opensavvy.formulaide.test.execution
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.test.TestScope
+import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -17,4 +20,36 @@ interface Suite {
 		block: suspend TestScope.() -> Unit,
 	)
 
+}
+
+internal class Test(val name: String) : AbstractCoroutineContextElement(Test) {
+
+	private val cacheLock = Mutex()
+	private val cache = HashMap<Any, Any>()
+
+	private val finalizerLock = Mutex()
+	private val finalizers = ArrayList<suspend TestScope.() -> Unit>()
+
+	suspend fun cacheValue(scope: TestScope, key: Any, compute: suspend TestScope.() -> Any) =
+		cacheLock.withLock { cache[key] } ?: run {
+			val result = with(scope) {
+				compute()
+			}
+
+			cacheLock.withLock { cache[key] = result }
+
+			result
+		}
+
+	suspend fun registerFinalizer(block: suspend TestScope.() -> Unit) = finalizerLock.withLock {
+		finalizers.add(block)
+	}
+
+	suspend fun TestScope.executeFinalizers() = finalizerLock.withLock {
+		for (finalizer in finalizers.asReversed()) {
+			finalizer()
+		}
+	}
+
+	companion object : CoroutineContext.Key<Test>
 }
