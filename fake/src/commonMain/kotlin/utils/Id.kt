7188dcdb160619fd1f58728342c75100ecb9399e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-04-23 19:02:07 +0200

    refactor(fake): Identifiers are integers instead of strings

--- fake/src/commonMain/kotlin/utils/Id.kt
+++ fake/src/commonMain/kotlin/utils/Id.kt
@@ -1,6 +1,5 @@
 package opensavvy.formulaide.fake.utils
 
 import kotlin.random.Random
-import kotlin.random.nextUInt
 
-internal fun newId(): String = Random.nextUInt().toString(16)
+internal fun newId(): Long = Random.nextLong()
