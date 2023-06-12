52e587638f26781f0a507b697266cac82ad9541e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-06-12 21:29:30 +0200

    tests: Simplify the guest masquerading

--- test/src/commonMain/kotlin/utils/Masquerade.kt
+++ test/src/commonMain/kotlin/utils/Masquerade.kt
@@ -3,6 +3,7 @@ package opensavvy.formulaide.test.utils
 import kotlinx.coroutines.withContext
 import opensavvy.backbone.now
 import opensavvy.formulaide.core.Auth
+import opensavvy.formulaide.core.Guest
 import opensavvy.formulaide.core.User
 import opensavvy.formulaide.core.User.Role.Companion.role
 import opensavvy.formulaide.test.structure.Setup
@@ -10,7 +11,7 @@ import opensavvy.formulaide.test.structure.TestScope
 import opensavvy.formulaide.test.structure.prepare
 
 suspend fun <T> executeAsGuest(block: suspend () -> T) =
-	withContext(Auth(User.Role.Guest, null)) { block() }
+	withContext(Guest) { block() }
 
 suspend fun <T> TestScope.executeAs(employee: User.Ref, block: suspend () -> T) =
 	withContext(Auth(employee.now().bind().role, employee)) { block() }
